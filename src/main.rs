d0bcd20f88f685f1f6dadd6d0b0bb6881737d138
kegan <keganericksen@live.com>
2026-09-16 16:14:09 -0600

    Added disk storage monitoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sysinfo::System;
+use sysinfo::{Disks, System};
 use std::thread;
 use std::time::Duration;
 
@@ -16,4 +16,19 @@ fn main() {
     println!("Total memory:  {} MB", system.total_memory() / 1024 / 1024);
     println!("Used memory:   {} MB", system.used_memory() / 1024 / 1024);
     println!("CPU usage:     {:.1}%", system.global_cpu_usage());
+
+    let disks = Disks::new_with_refreshed_list();
+
+    println!("\nStorage");
+    println!("-------");
+
+    for disk in disks.list() {
+        let total_gb = disk.total_space() / 1_000_000_000;
+        let available_gb = disk.available_space() / 1_000_000_000;
+        let used_gb = total_gb - available_gb;
+
+        println!(
+            "{}: {} GB used / {} GB total", disk.mount_point().display(), used_gb, total_gb
+        );
+    }
 }
